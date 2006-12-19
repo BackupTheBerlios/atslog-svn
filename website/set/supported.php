8134fe3baf3f73bc84ba236326f895704dd6f2c0
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2006-12-19 10:20:32 +0000

    information about supported models moved to supported.php file

---
+++ website/set/supported.php
@@ -0,0 +1,7 @@
+<li><u>Alcatel:</u> 4200E
+<li><u>GoldStar:</u> GPS-6A
+<li><u>Hybrex:</u> GD-320
+<li><u>LG:</u> GDK-100, GHX 46, LDK-100, LDK-300
+<li><u>Panasonic:</u> TA616RU, KX-TA308RU, KX-TA308, KX-TD1232, KX-TD500, KX-TD816RU
+<li><u>Samsung:</u> NX-820, SKP-816
+<li><u>Siemens:</u> Hicom 250, Hicom 350H
