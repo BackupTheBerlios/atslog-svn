9534a2078c27f834fc8920a001ea7b08715ac1f3
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2006-12-19 22:52:46 +0000

    news siemens added

--- website/set/supported.php
+++ website/set/supported.php
@@ -4,4 +4,4 @@
 <li><u>LG:</u> GDK-100, GHX 46, LDK-100, LDK-300
 <li><u>Panasonic:</u> TA616RU, KX-TA308RU, KX-TA308, KX-TD1232, KX-TD500, KX-TD816RU, KX-TEM824
 <li><u>Samsung:</u> NX-820, SKP-816
-<li><u>Siemens:</u> Hicom 250, Hicom 350H
+<li><u>Siemens:</u> Hicom 250, Hicom 350H, HiPath-3000/3750
