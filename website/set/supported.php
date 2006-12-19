fa1db53fb758a118f3b88e204829bebe336e4853
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2006-12-19 10:23:16 +0000

    added TEM824

--- website/set/supported.php
+++ website/set/supported.php
@@ -2,6 +2,6 @@
 <li><u>GoldStar:</u> GPS-6A
 <li><u>Hybrex:</u> GD-320
 <li><u>LG:</u> GDK-100, GHX 46, LDK-100, LDK-300
-<li><u>Panasonic:</u> TA616RU, KX-TA308RU, KX-TA308, KX-TD1232, KX-TD500, KX-TD816RU
+<li><u>Panasonic:</u> TA616RU, KX-TA308RU, KX-TA308, KX-TD1232, KX-TD500, KX-TD816RU, KX-TEM824
 <li><u>Samsung:</u> NX-820, SKP-816
 <li><u>Siemens:</u> Hicom 250, Hicom 350H
