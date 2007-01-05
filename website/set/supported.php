6be6a787b1e735f94fae4c306e1d7485801ee4bb
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2007-01-05 01:04:37 +0000

    Added support for the KX-TDA100/KX-TDA200

--- website/set/supported.php
+++ website/set/supported.php
@@ -2,6 +2,6 @@
 <li><u>GoldStar:</u> GPS-6A
 <li><u>Hybrex:</u> GD-320
 <li><u>LG:</u> GDK-100, GHX 46, LDK-100, LDK-300
-<li><u>Panasonic:</u> TA616RU, KX-TA308RU, KX-TA308, KX-TD1232, KX-TD500, KX-TD816RU, KX-TEM824
+<li><u>Panasonic:</u> TA616RU, KX-TA308RU, KX-TA308, KX-TD1232, KX-TD500, KX-TD816RU, KX-TEM824, KX-TDA100, KX-TDA200
 <li><u>Samsung:</u> NX-820, SKP-816, OfficeServ 7200
 <li><u>Siemens:</u> HiCom 150, HiCom 250, HiCom 350H, HiPath-3000/3750
