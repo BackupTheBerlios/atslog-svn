fc49e744fadc3fbb8a8b4a2fa783db86c3d027f8
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2007-01-13 07:36:13 +0000

    output to the file

--- trunk/libexec/genindex.sh
+++ trunk/libexec/genindex.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+LST="modules.lst"
 
+rm -f $LST
 for FILENAME in `ls *lib`
 do
-    echo $FILENAME:`grep 'MODELS: ' $FILENAME | sed 's/# MODELS: //'`:`grep 'VENDOR: ' $FILENAME | sed 's/# VENDOR: //'`
+    (echo $FILENAME:`grep 'MODELS: ' $FILENAME | sed 's/# MODELS: //'`:`grep 'VENDOR: ' $FILENAME | sed 's/# VENDOR: //'`)>>$LST
 done;
