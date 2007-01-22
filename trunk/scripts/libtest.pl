443c46c1dc8080190a0a592c94e68ed6fa88015c
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2007-01-22 15:43:24 +0000

    added total footer

--- trunk/scripts/libtest.pl
+++ trunk/scripts/libtest.pl
@@ -5,8 +5,11 @@ if (@ARGV[0] eq ''){
 }
 require @ARGV[0];
 open(PBX_DATA,"-");
+$incoming_total=0;
+$outgoing_total=0;
+$duration_total=0;
 parsecurcalls();
-
+print("total: out: $incoming_total, in: $outgoing_total, dur.: $duration_total\n");
 sub WriteRecord{
     my $time_of_call = $_[0];
     my $fwd = $_[1];
@@ -15,5 +18,9 @@ sub WriteRecord{
     my $way = $_[4];
     my $number = $_[5];
     my $duration = $_[6];
+    $duration_total+=$duration;
 print("LOG: ".$str."OUT: `$time_of_call`,`$fwd`,`$int`,`$co`,`$way`,`$number`,`$duration`\n\n");
+    if($way==2) {$incoming_total++;}
+    elsif($way==1) {$outgoing_total++;}
 }
+
