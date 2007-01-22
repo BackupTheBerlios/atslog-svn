cbfa9d7d0b44897035ec96f56734b43d55ec8a65
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2007-01-22 23:25:59 +0000

    libraty check added (uncomplete)

--- trunk/scripts/libtest.pl
+++ trunk/scripts/libtest.pl
@@ -3,13 +3,26 @@
 if (@ARGV[0] eq ''){
     die("USAGE: libtest <libname>\n");
 }
-require @ARGV[0];
-open(PBX_DATA,"-");
-$incoming_total=0;
-$outgoing_total=0;
-$duration_total=0;
-parsecurcalls();
-print("total: out: $incoming_total, in: $outgoing_total, dur.: $duration_total\n");
+
+open(IN,@ARGV[0]) || die print("Can't open library.\n");
+while($line=<IN>) {
+    if($line=~/# TESTLOG: (.+)\n?/){
+	print "@ARGV[0] $1\n";
+	open(PBX_DATA,"../textlogs/$1") || warn ("cannot open:$1");
+	readlog(@ARGV[0]);
+    }
+}
+close(IN);
+
+sub readlog{
+    require $_[0];
+    $incoming_total=0;
+    $outgoing_total=0;
+    $duration_total=0;
+    parsecurcalls();
+    print("total: out: $incoming_total, in: $outgoing_total, dur.: $duration_total\n\n");
+}
+
 sub WriteRecord{
     my $time_of_call = $_[0];
     my $fwd = $_[1];
@@ -19,8 +32,28 @@ sub WriteRecord{
     my $number = $_[5];
     my $duration = $_[6];
     $duration_total+=$duration;
-print("LOG: ".$str."OUT: `$time_of_call`,`$fwd`,`$int`,`$co`,`$way`,`$number`,`$duration`\n\n");
+    # print("LOG: ".$str."OUT: `$time_of_call`,`$fwd`,`$int`,`$co`,`$way`,`$number`,`$duration`\n\n");
     if($way==2) {$incoming_total++;}
     elsif($way==1) {$outgoing_total++;}
 }
 
+# ampto24 with russian and english language support
+sub AmPmTo24(){
+	my $return24='';
+	my $hour=$_[0];
+	my $AmPm=$_[1];
+	if ($AmPm eq 'PM' || $AmPm eq 'èè'){
+		if($hour < 12){
+			$return24=$hour+12;
+		}elsif($hour == 12){
+			$return24=12;
+		}
+	}elsif($AmPm eq 'AM' || $AmPm eq 'Ñè'){
+		if($hour < 12){
+			$return24=$hour;
+		}elsif($hour == 12){
+			$return24=0;
+		}
+	}
+	return $return24;
+}
