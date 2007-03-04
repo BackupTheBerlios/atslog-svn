d9b53bfd7baa36600f70e3d0ae7137e4010940ae
samm <samm@fadf18a1-93e7-0310-b2e0-c758d67b2163>
2007-03-04 09:06:41 +0000

    2.1.1 version

--- website/set/supported.php
+++ website/set/supported.php
@@ -1,9 +1,10 @@
-<?php $vendors_count=8; $models_count=31; $supported_text='<li><u>Alcatel:</u> 4200E
+<?php $vendors_count=9; $models_count=34; $supported_text='<li><u>Alcatel:</u> 4200E
+<li><u>Ericsson:</u> BP-250
 <li><u>Goldstar:</u> GPS-6A
 <li><u>Hybrex:</u> GD-320
 <li><u>LG:</u> GDK-100, GDK-162, GHX-46, LDK-100, LDK-300
-<li><u>NEC:</u> NEAX2000
-<li><u>Panasonic:</u> KX-TA308, KX-TA308RU, KX-TA616, KX-TA616RU, KX-TD1232, KX-TD1232-RU, KX-TD500, KX-TD816RU, KX-TDA100, KX-TDA200, KX-TEM824, KX-TES824
+<li><u>NEC:</u> NEAX2000, NEAX7400
+<li><u>Panasonic:</u> KX-TA308, KX-TA308RU, KX-TA616, KX-TA616RU, KX-TD1232, KX-TD1232-RU, KX-TD500, KX-TD816RU, KX-TDA100, KX-TDA200, KX-TDA600, KX-TEM824, KX-TES824
 <li><u>Samsung:</u> IDCS-500, NX-820, OFFICESERV-100, OFFICESERV-500, OFFICESERV-7200, SKP-816
 <li><u>Siemens:</u> HICOM-150, HIPATH-3750, Hicom 250, Hicom 350H
 '; ?>
